a31f48045acb0deb0398507f6188347979bd3edb
kylemzzy <113484938+kylemzzy@users.noreply.github.com>
2024-04-29 10:36:10 -0700

    Scene manager, crash time delay, new levels

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,25 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
-{
+{   
+    float timeDelay = 1f;
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag){
             case "Friendly":
                 Debug.Log("Friendly");
                 break;
-            case "Fuel":
-                Debug.Log("Fuel");
-                break;
             case "Finish":
                 Debug.Log("Finish");
+                LoadNextLevel();
                 break;
             default:
-                Debug.Log("DIED");
+                StartCrashSequence();
                 break;
             
         }
+    }
+    void StartCrashSequence(){
+        // there is a movement script inside the rocket. 
+        GetComponent<Movement>().enabled = false;
+        // calls the function AFTER 1 second float
+        Invoke("ReloadCurrentLevel", timeDelay);
+    }
+    void ReloadCurrentLevel(){
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+        GetComponent<Movement>().enabled = true;
+    }
+
+    void LoadNextLevel(){
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings){
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
