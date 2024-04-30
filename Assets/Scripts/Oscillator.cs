38b8c69cbdf988de524172c83adaab1e33c94baf
kylemzzy <113484938+kylemzzy@users.noreply.github.com>
2024-04-30 12:18:58 -0700

    added the oscillator

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
-    // we create TAU which is 2pi
+    // we create TAU which is 2pi (complete lap around circle)
     const float TAU = Mathf.PI * 2;
 
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,26 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // (NaN,Nan,Nan) error means we are dividing by 0
+        // WHY DO WE DO MATHf.Epsilon?
+        /*
+            idea is that we want to avoid the Nan error so we initially
+            would do something like if (period == 0f). This is BAD 
+            to compare floats with 0. this is because floats are SO precies
+            there may be chances where it does not equal 0 exact. 
+            Mathf.Epsilon is the smallest or known value closest to 0.
+            when checking against 0 for floats use <= Mathf.Epsilon :)
+        */
+        if(period <= Mathf.Epsilon) return;
+
+        float cycles = Time.time / period;
+        float rawSinWave = Mathf.Sin(cycles * TAU);
+        /*
+            rawSinWave = -1 to 1. 
+            Add 1 = 0 to 2 
+            Divide 2 = 0 to 1
+        */
+        movementFactor = (rawSinWave + 1f) / 2f; //instead of -1 to 1 we want 0 to1
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
